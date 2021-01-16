f1b44b0b2da4c1a4c7e93cf8a17aa0f988cb01c4
Diego Sanchez <diego.sanchez@etermax.com>
2021-01-16 16:23:08 -0300

    Overdrafting tests added

--- test/contactless_public_transport/use_case_test.clj
+++ test/contactless_public_transport/use_case_test.clj
@@ -1,14 +1,21 @@
 (ns contactless-public-transport.use-case-test
   (:require [clojure.test :refer :all]
             [contactless-public-transport.use_case :refer :all]))
+
 (def a-card-with-balance-one-hundred
   (create-card 100 0))
+
+(def a-card-with-balance-one-hundred-and-with-overdraft-of-ten
+  (create-card 100 10))
 
 (deftest successful-ride
   (testing "Given a card with balance of 100 and user taking a train costing 10 can get it"
     (is (:status
          (pay-ride a-card-with-balance-one-hundred
                    10)))
+    (is (:status
+         (pay-ride a-card-with-balance-one-hundred-and-with-overdraft-of-ten
+                   109)))
     (is (:status
          (pay-ride
           (:card (pay-ride (create-card 100 0) 10))
@@ -19,6 +26,9 @@
     (is (not (:status
               (pay-ride a-card-with-balance-one-hundred
                         110))))
+    (is (not (:status
+         (pay-ride a-card-with-balance-one-hundred-and-with-overdraft-of-ten
+                   120))))
     (is (not (:status
               (pay-ride
                (:card (pay-ride a-card-with-balance-one-hundred 10))
